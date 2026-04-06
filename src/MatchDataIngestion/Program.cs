33f08e29706b2c166b9d28a88885be88d2488ea4
Sezer Fatih <sezerfatih07@gmail.com>
2026-04-07 00:00:06 +0300

    Project structure created under /src folder. 4 service and shared created.

---
+++ src/MatchDataIngestion/Program.cs
@@ -0,0 +1,7 @@
+using MatchDataIngestion;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ src/ScoringWorker/Program.cs
@@ -0,0 +1,7 @@
+using ScoringWorker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
